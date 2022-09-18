293c29df5b2faaafcc3c769fa667773e96a82ffe
kab0 <v82drew@gmail.com>
2022-09-18 14:02:25 +0200

    this is also followed now.

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input("What is your name?") # assigning input to x
+# will make the input print out below.
+# ^ input will expect a string, that is text (so no int/float/complex
+print("Hello, " + name.title() + ".")
+# ^ I already know most of this.
+# pseudocode is for outlining the program you're coding.
+""""
+Many many many lines
+of comments.
+"""
